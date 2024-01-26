8abf58c520c37e9ce048d82cf5dbfe37019da1c7
Wahtu Astrawan <chatt699@gmail.com>
2024-01-27 00:05:58 +0800

    fixing bug url validation and passed the test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI)
@@ -27,18 +28,28 @@ const urlSchema = new mongoose.Schema({
 
 const UrlModel = mongoose.model("Url", urlSchema);
 
-const isValidURL = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
+const isValidURL = async (url) => {
+  return new Promise((resolve) => {
+    try {
+      const parsedURL = new URL(url);
+      const protocol = parsedURL.protocol.startsWith("http") ? "" : "http://";
+      dns.lookup(`${protocol}${parsedURL.host}`, (err) => {
+        resolve(!err);
+      });
+    } catch (error) {
+      resolve(false);
+    }
+  });
 }
 
-const urlChecker = (req, res, next) => {
-  if (isValidURL(req.body.url)) next();
-  else res.status(400).json({ error: "invalid url" });
+const urlChecker = async (req, res, next) => {
+  const url = req.body.url;
+
+  if (url && (await isValidURL(url))) {
+    next();
+  } else {
+    return res.json({ error: "Invalid URL" });
+  }
 }
 
 const generateRandomNum = () => {
@@ -71,16 +82,16 @@ app.post('/api/shorturl', urlChecker, async (req, res) => {
 
   await UrlModel.create({ original_url: url, short_url: shortenerId });
 
-  res.json({ original_url: `${url}`, short_url: shortenerId });
+  return res.json({ original_url: `${url}`, short_url: shortenerId });
 });
 
 app.get('/api/shorturl/:shortId', async (req, res) => {
   const shortId = req.params.shortId;
   const urlData = await UrlModel.findOne({ short_url: shortId });
   if (urlData) {
-    res.redirect(urlData.original_url);
+    return res.redirect(urlData.original_url);
   } else {
-    res.json({ error: "short url not valid" });
+    return res.json({ error: "No short URL found for the given input" });
   }
 });
 
